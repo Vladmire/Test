ee339b075b917cfce62507a2b60cdc7f6c0e96ef
Vladimir Dubovik <dubo@sfedu.ru>
2022-05-12 16:51:09 +0300

    Initial Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by imac44 on 12.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
