eb19d9e7c1fb8954a92c8eeeb9a14eb079e595e3
Vladimir Dubovik <dubo@sfedu.ru>
2022-05-18 16:07:32 +0300

    make a normal displaying of items in detailed view controller

---
+++ test/Helpers/ViewsFactory.swift
@@ -0,0 +1,8 @@
+//
+//  ViewsFactory.swift
+//  test
+//
+//  Created by imac44 on 18.05.2022.
+//
+
+import Foundation
---
+++ test/View/DetailedView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailedView.swift
+//  test
+//
+//  Created by imac44 on 18.05.2022.
+//
+
+import UIKit
+
+class DetailedView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
